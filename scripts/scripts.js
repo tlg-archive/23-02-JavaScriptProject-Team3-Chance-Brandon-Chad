65d4877e3625528b4a49e8fe18fee838a7377054
Brandon(Suin) Park <Suin.Park@tlgcohort.com>
2023-04-10 16:57:26 -0700

    small updates on index.html update on weather.js and scripts.js
    Co-authored-by: Chad Kellum <chadkellum@users.noreply.github.com>
    Co-authored-by: Chance Jolley <CodeChanceCode@users.noreply.github.com>

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,29 @@
 import { fetchWeatherData } from "scripts/weather.js";
 import { selectActivities } from "scripts/activities.js";
 import { handleDateSelection } from "scripts/calendar.js";
+
+const clickSearch = document.getElementById("submit_button");
+
+const output_box_sections = document.createElement("div");
+output_box_sections.innerHTML = `<section id="forecast"></section>
+<section id="activities_button"></section>
+<section id="suggested_box"></section>
+</section>`;
+
+output_box.appendChild(output_box_sections);
+
+clickSearch.addEventListener("click", fetchWeatherData());
+user_input_form.reset();
+
+const activities_clicked = document.getElementById("activities_button");
+const activities_container = document.createElement("div");
+activities_container.innerHTML = `<button id="indoor_button" class="btn btn-secondary">Indoor Activities</button>
+<button id="outdoor_button" class="btn btn-warning">Outdoor Activities</button>
+<button id="all_button" class="btn btn-success">All Activities</button>
+`;
+output_box.appendChild(activities_container);
+activities_clicked.addEventListener("click", clickSearch);
+
+const suggested_clicked = document.getElementById("suggested_box");
+const suggested_container = document.createElement("div");
+suggested_container;
--- scripts/weather.js
+++ scripts/weather.js
@@ -1,1 +1,81 @@
-export async function fetchWeatherData(location, days) {}
+// EventListener for form submission
+user_input_form.addEventListener("submit", fetchWeatherData);
+
+// function to fetch weather data based on user input(location and days)
+export async function fetchWeatherData(e) {
+  e.preventDefault();
+
+  const userLocation = document.getElementById("location_name").value;
+  const userDay = document.getElementById("date_selection").value;
+
+  const m3o = require("m3o").default(
+    process.env.M3O_ODYzYTdmY2EtYzBiMi00ZTRmLWIwZTUtNmZlZjRmZDFlMDJh
+  );
+
+  let rsp = await m3o.weather.forecast({
+    days: userDay,
+    location: userLocation,
+  });
+
+  const forecastData = rsp.forecasts.find((item) => {
+    return item.location.name.toLowerCase() === userLocation.toLowerCase();
+  });
+
+  user_input_form.reset();
+
+  const temperature = forecastData.avg_temp_f;
+  const description = forecastData.condition;
+  const iconUrl = forecastData.icon_url;
+  const chanceOfRain = forecastData.chance_of_rain;
+  const windSpeed = forecastData.max_wind_mph;
+
+  const forecastSection = document.querySelector("#forecast");
+  forecastSection.innerHTML = `
+    <img src="${iconUrl}" alt="Weather icon">
+    <h2>Weather forecast for ${userLocation}:</h2>
+    <p>Temperature: ${temperature}°F</p>    
+    <p>Chance of rain: ${chanceOfRain}%</p>
+    <p>Description: ${description}</p>
+    <p>Max Wind Speed: ${windSpeed} mph</p>
+  `;
+}
+
+// Fetching Weather API
+//   const apiUrl = "https://api.m3o.com/v1/weather/Forecast";
+//   const apiKey = "ODYzYTdmY2EtYzBiMi00ZTRmLWIwZTUtNmZlZjRmZDFlMDJh";
+
+//   const response = await fetch(apiUrl, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${apiKey}`,
+//     },
+//     body: JSON.stringify({
+//       days: userDay,
+//       location: userLocation,
+//     }),
+//   });
+
+//   const data = await response.json();
+
+// Extracting the data for the user's location and day
+//   const forecastData = data.forecasts.find((item) => {
+//     return item.location.name.toLowerCase() === userLocation.toLowerCase();
+//   });
+
+//   const temperature = forecastData.avg_temp_f;
+//   const description = forecastData.condition;
+//   const iconUrl = forecastData.icon_url;
+//   const chanceOfRain = forecastData.chance_of_rain;
+//   const windSpeed = forecastData.max_wind_mph;
+
+//   const forecastSection = document.querySelector("#forecast");
+//   forecastSection.innerHTML = `
+//     <img src="${iconUrl}" alt="Weather icon">
+//     <h2>Weather forecast for ${userLocation}:</h2>
+//     <p>Temperature: ${temperature}°F</p>
+//     <p>Chance of rain: ${chanceOfRain}%</p>
+//     <p>Description: ${description}</p>
+//     <p>Max Wind Speed: ${windSpeed}</p>
+//   `;
+// }
