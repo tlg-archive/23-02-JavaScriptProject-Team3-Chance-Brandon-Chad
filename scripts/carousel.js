10dd032c85e9681d12f86da95737f9aa65c12cb9
Brandon(Suin) Park <Suin.Park@tlgcohort.com>
2023-04-13 12:04:57 -0700

    working carousel Co-authored-by: Chad Kellum <chadkellum@users.noreply.github.com> Co-authored-by: Chance Jolley <CodeChanceCode@users.noreply.github.com>

--- scripts/carousel.js
+++
@@ -1,5 +0,0 @@
-import { selectActivities } from "./activities.js";
-import { activities } from "./activities.js";
-// for each filtered result that fits the criteria of weather
-// create a carousel card with subheading and description(link)
-// also needs to create arrow buttons to scroll between the suggested Activities
