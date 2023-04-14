40f4e797f997d8b66c5a34c4f970a8e3e367b18e
Brandon(Suin) Park <Suin.Park@tlgcohort.com>
2023-04-13 17:27:22 -0700

    working carousel, finished css and touch up on js
    Co-authored-by: Chad Kellum <chadkellum@users.noreply.github.com>
    Co-authored-by: Chance Jolley <CodeChanceCode@users.noreply.github.com>

--- scripts/main.js
+++ scripts/main.js
@@ -27,32 +27,31 @@ async function output(e) {
   //weather forecast output section
   const output_box_forecast = document.createElement("div");
   output_box_forecast.innerHTML = `
-    <section id="forecast">
+    <section id="forecast">      
       <h2> ${fetchWeatherData.userCity} on ${fetchWeatherData.userDate} </h2>
       <img id ="icon" src=${fetchWeatherData.iconUrl}>
       <div id ="temperature"><p>Temperature: ${fetchWeatherData.temperature}</p></div>
       <div id ="description"><p>Description: ${fetchWeatherData.description}</p></div>
       <div id ="windSpeed"><p>Wind Speed: ${fetchWeatherData.windSpeed}</p></div>
       <div id ="chanceOfRain"><p>Chance of rain: ${fetchWeatherData.chanceOfRain}</p></div>
-    </section>`;
+    </section><br><br>`;
 
-  // const output_box_carousel = document.createElement("div");
-  // output_box_carousel.setAttribute("id", "carouselExampleIndicators");
-  // output_box_carousel.setAttribute("class", "carousel slide");
-  // output_box_carousel.innerHTML = `
-  // <div id="carousel_inner" class="carousel-inner"></div>
-
-  //   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-  //     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-  //     <span class="visually-hidden">Previous</span>
-  //   </button>
-  //   <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-  //     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-  //     <span class="visually-hidden">Next</span>
-  //   </button>
-  // </div>`;
   output_box.appendChild(output_box_forecast);
-  // output_box.appendChild(output_box_carousel);
+  // carousel of activities output section
+  const output_box_activities = document.createElement("div");
+  output_box_activities.innerHTML = `
+  <div id="carouselExampleIndicators" class="carousel slide">
+          <div id="carousel_inner" class="carousel-inner"></div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>`;
+  output_box.appendChild(output_box_activities);
 
   //carousel items of random-filtered activities
   randomActivities.forEach((activity, currentIdx) => {
@@ -63,7 +62,7 @@ async function output(e) {
       activityElement.className = "carousel-item";
     }
     activityElement.innerHTML = `
-      <img src="${activity.image}" class= "d-block w-100" alt="${activity.name}">
+      <img src="${activity.image}" class= "d-block w-100" alt="${activity.name}" style="max-height:700px; object-fit:cover;">
       <div class= "carousel-caption d-none d-md-block">
       <h5>${activity.name}</h5>
       `;
